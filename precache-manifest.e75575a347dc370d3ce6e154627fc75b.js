32d19aa9a915ab5c8640e8452855ce79036093f7
Ali <bagaliasger53@gmail.com>
2022-06-28 22:09:53 +0530

    Updates

---
+++ precache-manifest.e75575a347dc370d3ce6e154627fc75b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e62262cc34abf0c3a5a891c33a30f2",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "110e1a8b8c08fe8d5c05",
+    "url": "/reactlivewebsite/static/css/2.a85e5442.chunk.css"
+  },
+  {
+    "revision": "f173f2586c05364681a1",
+    "url": "/reactlivewebsite/static/css/main.d482fd81.chunk.css"
+  },
+  {
+    "revision": "110e1a8b8c08fe8d5c05",
+    "url": "/reactlivewebsite/static/js/2.0ff54afb.chunk.js"
+  },
+  {
+    "revision": "b05e69d912d545c80f446573595484ef",
+    "url": "/reactlivewebsite/static/js/2.0ff54afb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f173f2586c05364681a1",
+    "url": "/reactlivewebsite/static/js/main.898f20cf.chunk.js"
+  },
+  {
+    "revision": "48f7b2efd50006fcc042",
+    "url": "/reactlivewebsite/static/js/runtime-main.355cd751.js"
+  },
+  {
+    "revision": "18e9b7ad9e3e4072ee8ea10761a403c2",
+    "url": "/reactlivewebsite/static/media/27.18e9b7ad.svg"
+  },
+  {
+    "revision": "a3e117461eb656fd7017a84673c7a792",
+    "url": "/reactlivewebsite/static/media/img1.a3e11746.jpg"
+  },
+  {
+    "revision": "8d8e8debde507e27eefffd68c295b510",
+    "url": "/reactlivewebsite/static/media/img2.8d8e8deb.jpg"
+  },
+  {
+    "revision": "28b28678eb44d6033326abf5ba17cb46",
+    "url": "/reactlivewebsite/static/media/img3.28b28678.jpg"
+  },
+  {
+    "revision": "fd3178d4613707d8c4fcef6a29eab377",
+    "url": "/reactlivewebsite/static/media/img4.fd3178d4.jpg"
+  },
+  {
+    "revision": "c15d1942acfffab070ddec0579b141b5",
+    "url": "/reactlivewebsite/static/media/img5.c15d1942.jpg"
+  },
+  {
+    "revision": "8dc77972b874c467db6ee9d22d3ce129",
+    "url": "/reactlivewebsite/static/media/img6.8dc77972.jpg"
+  },
+  {
+    "revision": "3fb292e950fcab977151d7b082cc60fa",
+    "url": "/reactlivewebsite/static/media/reactimg.3fb292e9.svg"
+  },
+  {
+    "revision": "f481cc456f999011f807c3df248bc4a9",
+    "url": "/reactlivewebsite/static/media/toggle.f481cc45.svg"
+  }
+]);
